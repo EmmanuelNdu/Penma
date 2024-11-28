052e6bbdd0d18872297a37595f2abd2aa883f3c5
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-28 01:19:58 +0100

    Navbar

---
+++ src/constants/index.jsx
@@ -0,0 +1,16 @@
+export const navItems = [
+    { label: "Personal", href:"#" },
+    { label: "Business", href:"#" },
+    { label: "Company", href:"#" },
+    { label: "Help", href:"#" },
+];
+
+export const navItems_m = [
+    { label: "Personal", href:"#" },
+    { label: "Business", href:"#" },
+    { label: "Company", href:"#" },
+    { label: "Developers", href:"#" },
+    {label: "Contact Us", href:"#" },
+    {label: "Help", href:"#" },
+  
+];
