1f934be587de57edcec209754926daa03f3fa26b
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-28 16:12:37 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,26 +6,28 @@ const Navbar = () => {
 
   // const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false)
 
-    const toggleNavbar = () => {
-        setMobileDrawerOpen( !mobileDrawerOpen);
-    }
+  // const toggleNavbar = () => {
+  //     setMobileDrawerOpen( !mobileDrawerOpen);
+  // }
 
   return (
-    <nav className='sticky top-0 z-50 py-3  bg-white'>
-        <div className='container px-4 mx-auto relative text-sm text-black'>
-          <div className='flex justify-between items-center text-base'>
-            <div className='flex items-center flex-shrink-0'>
-              <img className="h-15 w-20 ml-4" src={logo} alt="logo" />
-              <ul className='hidden lg:flex ml-14 space-x-12 font font-medium'>
-                {navItems.map((item, index) => (
-                  <li key={index}>
-                    <a href={item.href}>{item.label}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+    <nav className='sticky top-0 z-50 py-8 backdrop-blur-lg border-b border-neutral-700/80'>
+      <div className='container px-4 mx-auto relative text-sm'>
+        <div className='flex justify-center items-center'>
+          <div className='flex items-center flex-shrink-0'>
+            <img className='h-10 w-10 mr-2' src={logo} alt='logo' />
           </div>
+          <ul className='hidden lg:flex ml-14 space-x-12'>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            )
+
+          )}
+          </ul>
         </div>
+      </div>
     </nav>
   )
 }
